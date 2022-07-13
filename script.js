8cec9c93d567cbb77f7877fa842b023c1f6e98b0
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-13 16:14:46 -0300

    requisito 3 feito

---
+++ script.js
@@ -0,0 +1,15 @@
+const botaoHeader = document.getElementById("btn-header");
+
+const email = document.getElementById("email-header");
+
+const senha = document.getElementById("senha-header");
+
+function validarBotao() {
+  if (email.value !== "tryber@teste.com" && senha.value !== "123456") {
+    alert("Email ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+}
+
+botaoHeader.addEventListener("click", validarBotao);
